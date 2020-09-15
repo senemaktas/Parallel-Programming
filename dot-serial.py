f06724d207ef5b5a7bc5fc3c2121c3f83333e0c9
senemaktas <30393152+senemaktas@users.noreply.github.com>
2020-09-15 14:31:23 +0300

    Update dot-serial.py

--- dot-serial.py
+++ dot-serial.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Dec 25 11:45:28 2017
-
-@author: dell
-"""
 import numpy
+
 #import sys
 #n = int(sys.argv[1])
+
 n=100
 dot = numpy.array([0.])
 x = numpy.linspace(0,100,n)
