328b7292d056716153637dd18d5cca0a1dfad460
senemaktas <30393152+senemaktas@users.noreply.github.com>
2020-09-15 14:30:27 +0300

    Update rand1.py

--- rand1.py
+++ rand1.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Nov 21 13:31:47 2018
-
-@author: dell
-"""
 
 import random
 import matplotlib.pyplot as plt
+
 print(random.randint(0,100))
 x=[random.randint(0,9) for p in range(0,9)]
 y=[random.randint(0,9) for p in range(0,9)]
